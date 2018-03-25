cccaeb6272d04f34f8961648f6ffba740ec541c7
Wataru Yahiro <wataru@WatarunoMacBook-puro.local>
2018-03-26 00:21:44 +0900

    Improve jump

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::io::{self, Write, Read};
+use std::collections::HashMap;
 
 struct Machine {
     pub pc: usize,
     pub ptr: usize,
-    pub mem: Vec<u8>
+    pub mem: Vec<u8>,
+    pub code: Vec<u8>,
+    pub map: HashMap<usize, usize>
 }
 
 impl Machine {
@@ -11,64 +14,77 @@ impl Machine {
         Machine {
             pc: 0,
             ptr: 0,
-            mem: Vec::new()
+            mem: Vec::new(),
+            code: Vec::new(),
+            map: HashMap::new()
         }
     }
 
-    fn run(&mut self, code: &[u8]) {
+    fn run(&mut self, code: &[u8]) -> Option<String> {
         self.pc = 0;
         self.ptr = 0;
         self.mem.clear();
+        self.code = Vec::from(code);
+        self.map = match self.init_map() {
+            Some(map) => { map },
+            None => { return Some(String::from("Unclosed brackets.")); }
+        };
 
-        while self.pc < code.len() {
+        while self.pc < self.code.len() {
             if self.mem.len() <= self.ptr { self.mem.push(0); }
 
-            match code[self.pc] {
-                b'>' => { self.ptr += 1 },
-                b'<' => { self.ptr -= 1 },
-                b'+' => { self.mem[self.ptr] += 1 },
-                b'-' => { self.mem[self.ptr] -= 1 },
-                b'.' => { print!("{}", self.mem[self.ptr] as char) },
-                b',' => { self.mem[self.ptr] = getchar() },
+            match self.code[self.pc] {
+                b'>' => { self.ptr += 1; },
+                b'<' => { self.ptr -= 1; },
+                b'+' => { self.mem[self.ptr] += 1; },
+                b'-' => { self.mem[self.ptr] -= 1; },
+                b'.' => { print!("{}", self.mem[self.ptr] as char); },
+                b',' => { self.mem[self.ptr] = getchar(); },
                 b'[' | b']' => {},
-                c => {
-                    println!("Unexpected character {}", c as char);
-                    break;
-                }
+                c => { return Some(format!("Unexpected charanter {}.", c as char)); }
             }
 
-            self.pc = self.next_pc(code);
+            self.pc = match self.next_pc() {
+                Some(pc) => { pc },
+                None => { return Some(String::from("Invalid PC.")); }
+            }
         }
+
+        None
     }
 
-    fn next_pc(&self, code: &[u8]) -> usize{
-        match code[self.pc] {
+    fn init_map(&self) -> Option<HashMap<usize, usize>> {
+        let mut map = HashMap::new();
+        let mut stk = Vec::new();
+        for i in 0..self.code.len() {
+            match self.code[i] {
+                b'[' => { stk.push(i); },
+                b']' => {
+                    match stk.pop() {
+                        Some(j) => {
+                            map.insert(i, j + 1);
+                            map.insert(j, i + 1);
+                        },
+                        None => { return None; }
+                    }
+                },
+                _ => {}
+            }
+        }
+
+        if stk.is_empty() { Some(map) } else { None }
+    }
+
+    fn next_pc(&self) -> Option<usize> {
+        match self.code[self.pc] {
             b'[' => {
-                if self.mem[self.ptr] == 0 { self.jump(code, b'[', b']',  1) } else { self.pc + 1 }
+                if self.mem[self.ptr] == 0 { self.map.get(&self.pc).cloned() } else { Some(self.pc + 1) }
             },
             b']' => {
-                if self.mem[self.ptr] != 0 { self.jump(code, b']', b'[', -1) } else { self.pc + 1 }
+                if self.mem[self.ptr] != 0 { self.map.get(&self.pc).cloned() } else { Some(self.pc + 1) }
             },
-            _ => {self.pc + 1}
+            _ => { Some(self.pc + 1) }
         }
-    }
-
-    fn jump(&self, code: &[u8], open: u8, close: u8, d: i32) -> usize {
-        let mut pc = self.pc;
-        let mut level = 0;
-        while pc < code.len() {
-            if code[pc] == open {
-                level += 1;
-            } else if code[pc] == close {
-                level -= 1;
-            }
-
-            if level == 0 { return pc + 1; }
-
-            pc = (pc as i32 + d) as usize;
-        }
-
-        panic!("unlosed brackets.");
     }
 }
 
@@ -96,6 +112,9 @@ fn main() {
             Err(e) => println!("{}", e)
         }
 
-        machine.run(line.trim().as_bytes());
+        match machine.run(line.trim().as_bytes()) {
+            Some(s) => { println!("{}", s); },
+            None => {}
+        }
     }
 }
